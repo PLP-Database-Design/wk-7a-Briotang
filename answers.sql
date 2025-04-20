7515463a52e7c0eb48736a62c8459287a3789608
Briotang <Tangpeeep@gmail.com>
2025-04-20 18:10:08 +0300

    assignment submission

---
+++ answers.sql
@@ -0,0 +1,44 @@
+-- question 1
+CREATE TABLE ProductDetail (
+    OrderID INT,
+    CustomerName VARCHAR(100),
+    Products VARCHAR(100)
+);
+INSERT INTO ProductDetail(OrderID, CustomerName, Products)
+VALUES
+(101, "John Doe", "Laptop"),
+(101, "John Doe", "Mouse"),
+(102, "Jane Smith", "Tablet"),
+(102, "Jane Smith", "Keyboard"),
+(102, "Jane Smith", "Mouse"),
+(103, "Emily Clark", "Phone");
+
+-- question 2
+CREATE TABLE orders (
+    OrderID INT PRIMARY KEY,
+    CustomerName VARCHAR(100)
+);
+INSERT INTO orders(OrderID, CustomerName)
+VALUES
+(101, "John Doe"),
+(102, "Jane Smith"),
+(103, "Emily Clark");
+
+
+CREATE TABLE orderitems (
+    OrderID INT,
+    Product VARCHAR(100),
+    Quantity INT,
+    FOREIGN KEY (OrderID)
+        REFERENCES orders (OrderID)
+);
+
+INSERT INTO orderitems(OrderID,Product, Quantity)
+VALUES
+(101, "Laptop", 2),
+(101, "Mouse", 1),
+(102, "Tablet", 3),
+(102, "Keyboard", 1),
+(102, "Mouse", 2),
+(103, "Phone", 1);
+
